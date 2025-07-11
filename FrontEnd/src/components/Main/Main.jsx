b82dffb5939976af3ce95ba06207ad31c01d9c92
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-07-12 00:32:50 +0530

    Main section Done.!

--- FrontEnd/src/components/Main/Main.jsx
+++ FrontEnd/src/components/Main/Main.jsx
@@ -1,58 +1,53 @@
-import React, {useRef} from "react";
-import Carousel from "../Carousel/Carousel";
-import main_bg_img from "../../assets/main-bg_img.jpg";
-import "./Main.css";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
+import main_img_1 from "../../assets/main_carousel1.jpg";
+import main_img_2 from "../../assets/main_carousel5.jpeg";
+import main_img_3 from "../../assets/main_carousel9.webp";
+import "./Main.css";
+import Carousel from "../Carousel/Carousel";
 import MidSection from "../MidSection/MidSection";
 
 const Main = () => {
   const theme = useSelector((state) => state.theme.value);
 
-//  ${!theme ? "bg-[#fce7f3]" : "bg-[#2e1a24] text-white"}
+  //  ${!theme ? "bg-[#fce7f3]" : "bg-[#2e1a24] text-white"}
 
   return (
     <div className={``}>
-      <div
-        className={`relative `}
-      >
+      <div className={` `}>
+        {/* <img className="main_bg" src={main_bg_img} alt="" /> */}
 
-        <img className="main_bg" src={main_bg_img} alt="" />
-
-        <div className="main-content">
+        <div className="main-content h-screen md:translate-y-[-5%] translate-y-[-5%]  bg-gradient-to-b from-white to-blue-50">
           <div className="flex flex-col items-start main_1 ">
-            <h1 className="font-bold main_1-head">
+            <span className="font-bold lg:text-6xl md:text-4xl main-head">
               Welcome to Resume Builder
-            </h1>
-            <p className="w-90 font-bold mt-4 ">
+            </span>
+            <p className="font-bold mt-4 lg:text-xl md:text-sm text-xs text-justify ">
               Craft a professional, job-winning resume in minutes! Our
               AI-powered resume builder tailors content, optimizes layouts, and
-              helps you stand out from the crowd. No more guesswork—just a
-              sleek, ATS-friendly resume that gets results. ✨ Start creating
-              your perfect resume today!
+              helps you stand out from the crowd. Start creating your perfect
+              resume today!
             </p>
-            <button className="bg-blue-500 hover:bg-blue-700 hover:scale-125 ease-in-out duration-300 text-white font-bold py-2 px-4 rounded mt-4">
+            <button className="bg-blue-500 hover:bg-blue-700 md:hover:scale-125 ease-in-out duration-300 text-white md:text-base text-xs font-bold py-2 px-4 rounded mt-4">
               Get Started
             </button>
           </div>
-          
-          <div className="main_2 lg:block md:block hidden">
-            {/* <img
-              src={BrandLogo}
-              style={{
-                height: "120px",
-                filter: theme ? "invert(1)" : "invert(0)", // Invert colors in dark mode
-                transition: "filter 0.3s ease-in-out",
-              }}
-              alt="Logo"
-            /> */}
+
+          <div className="main_2 relative md:flex ">
+            <img className="main-imgs" src={main_img_3} alt="" />
+            <img
+              className="main-imgs middle shadow-2xl "
+              src={main_img_1}
+              alt=""
+            />
+            <img className="main-imgs" src={main_img_2} alt="" />
           </div>
         </div>
       </div>
 
-      <MidSection />
+      {/* <MidSection /> */}
 
-      <Carousel />
-
+      {/* <Carousel /> */}
     </div>
   );
 };
